a96c9ff4d67e38432ec3d20bebd79714ee0dc36e
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-07-12 23:10:17 -0300

    add test for formatted output

--- screenmatch/src/main/java/br/com/coutsoft/screenmatch/main/Main.java
+++ screenmatch/src/main/java/br/com/coutsoft/screenmatch/main/Main.java
@@ -1,10 +1,13 @@
 package br.com.coutsoft.screenmatch.main;
 
+import br.com.coutsoft.screenmatch.model.SeriesData;
 import br.com.coutsoft.screenmatch.service.APIConsumer;
+import br.com.coutsoft.screenmatch.service.DataConverter;
 
 public class Main {
     public static void main(String[] args) {
         APIConsumer consumer = new APIConsumer();
-        System.out.println(consumer.consume("friends"));
+        DataConverter converter = new DataConverter();
+        System.out.println(converter.convert(consumer.consume("friends"), SeriesData.class));
     }
 }
