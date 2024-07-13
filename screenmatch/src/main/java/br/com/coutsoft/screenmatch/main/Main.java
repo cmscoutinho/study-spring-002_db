2e1e79fce894395222252aac9f16958aa975f6bf
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-07-12 22:50:42 -0300

    add jackson dependency

--- screenmatch/src/main/java/br/com/coutsoft/screenmatch/main/Main.java
+++ screenmatch/src/main/java/br/com/coutsoft/screenmatch/main/Main.java
@@ -1,7 +1,10 @@
 package br.com.coutsoft.screenmatch.main;
 
+import br.com.coutsoft.screenmatch.service.APIConsumer;
+
 public class Main {
     public static void main(String[] args) {
-
+        APIConsumer consumer = new APIConsumer();
+        System.out.println(consumer.consume("friends"));
     }
 }
