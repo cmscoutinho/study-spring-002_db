132ed514f0af5c63e14cc9d353a180bc40ec049f
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-07-19 21:31:42 -0300

    feat: create constructor on Series

--- screenmatch/src/main/java/br/com/coutsoft/screenmatch/model/Series.java
+++ screenmatch/src/main/java/br/com/coutsoft/screenmatch/model/Series.java
@@ -2,6 +2,8 @@ package br.com.coutsoft.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 
+import java.util.OptionalDouble;
+
 public class Series {
 
     private String title;
@@ -12,5 +14,12 @@ public class Series {
     private Double rating;
     private String poster;
     private String plot;
+
+    public Series(SeriesData seriesData) {
+        this.title = seriesData.title();
+        this.seasons = seriesData.seasons();
+        this.rating = OptionalDouble.of(Double.valueOf(seriesData.rating())).orElse(0);
+
+    }
 
 }
