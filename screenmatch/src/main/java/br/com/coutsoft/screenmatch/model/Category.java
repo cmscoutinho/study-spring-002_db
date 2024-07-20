3490ab4da7a47101aa182fe4cf867d600c21ee41
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-07-19 21:27:29 -0300

    feat: create enum Category

--- screenmatch/src/main/java/br/com/coutsoft/screenmatch/model/Category.java
+++ screenmatch/src/main/java/br/com/coutsoft/screenmatch/model/Category.java
@@ -1,4 +1,15 @@
 package br.com.coutsoft.screenmatch.model;
 
 public enum Category {
+    ACTION("Action"),
+    ROMANCE("Romance"),
+    COMEDY("Comedy"),
+    DRAMA("Drama"),
+    CRIME("Crime");
+
+    private String omdbCategory;
+
+    Category(String omdbCategory) {
+        this.omdbCategory = omdbCategory;
+    }
 }
