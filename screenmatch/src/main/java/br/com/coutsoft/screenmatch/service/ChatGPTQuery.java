bdea499825a830547e234edfabd30108390cf2ac
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-07-21 21:17:18 -0300

    feat: create ChatGPTQuery for dynamic translations of the movie plots

---
+++ screenmatch/src/main/java/br/com/coutsoft/screenmatch/service/ChatGPTQuery.java
@@ -0,0 +1,40 @@
+package br.com.coutsoft.screenmatch.service;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ChatGPTQuery {
+
+    private static String getEnvKey() {
+        Properties properties = new Properties();
+        String apiKey = "";
+        try {
+            properties.load(new FileInputStream(".env"));
+            apiKey = properties.getProperty("GPT3_KEY");
+//            System.out.println("API Key: " + apiKey);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return apiKey;
+    }
+
+    public static String getTranslation(String lang, String text) {
+        OpenAiService service = new OpenAiService(getEnvKey());
+
+        CompletionRequest request = CompletionRequest.builder()
+                .model("gpt-4o-mini-2024-07-18")
+                .prompt("please translate the following text to " + lang + ": " + text)
+                .maxTokens(1000)
+                .temperature(0.7)
+                .build();
+
+        var response = service.createCompletion(request);
+        return response.getChoices().get(0).getText();
+    }
+}
