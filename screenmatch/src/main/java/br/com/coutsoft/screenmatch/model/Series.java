a149c527651dc3650ee53b7137d08a44da10b3f6
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-07-29 23:27:24 -0300

    refactor: Series' @OneToMany set to cascade

--- screenmatch/src/main/java/br/com/coutsoft/screenmatch/model/Series.java
+++ screenmatch/src/main/java/br/com/coutsoft/screenmatch/model/Series.java
@@ -25,7 +25,7 @@ public class Series {
     private String poster;
     private String plot;
 
-    @OneToMany(mappedBy = "series")
+    @OneToMany(mappedBy = "series", cascade = CascadeType.ALL)
     private List<Episode> episodes = new ArrayList<>();
 
     public Series() {
