6a3be0564293897a11bfd0045d48772950ff1e25
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-08-09 22:07:58 -0300

    feat: start implementing derived query

--- screenmatch/src/main/java/br/com/coutsoft/screenmatch/repository/SeriesRepository.java
+++ screenmatch/src/main/java/br/com/coutsoft/screenmatch/repository/SeriesRepository.java
@@ -3,5 +3,8 @@ package br.com.coutsoft.screenmatch.repository;
 import br.com.coutsoft.screenmatch.model.Series;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SeriesRepository extends JpaRepository<Series, Long> {
+    Optional<Series> findByTitleIgnoreCase(String title);
 }
