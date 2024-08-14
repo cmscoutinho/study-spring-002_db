f3fdc582e36514d2471b23559ca73720c739a693
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-08-14 14:50:52 -0300

    feat: place basic properties of the main window

--- screenmatch/src/main/java/br/com/coutsoft/screenmatch/view/MainWindow.java
+++ screenmatch/src/main/java/br/com/coutsoft/screenmatch/view/MainWindow.java
@@ -1,4 +1,15 @@
 package br.com.coutsoft.screenmatch.view;
 
-public class MainWindow {
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow(String title) {
+        this.setTitle(title);
+
+        this.setSize(500, 400);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
 }
